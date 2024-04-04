33e1164519478de90d749b16c553f6ab64710689
NehaDhanaram <165715185+NehaDhanaram@users.noreply.github.com>
2024-04-04 23:36:28 +0530

    Update Covid Tableau Portfolio Project SQL Queries.sql

--- Covid Tableau Portfolio Project SQL Queries.sql
+++ Covid Tableau Portfolio Project SQL Queries.sql
@@ -30,7 +30,6 @@ order by TotalDeathCount desc
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
